154e8057702e2a9de08074cec38bc71b474c4d95
pabloacvd <pablo@xeven.com.ar>
2021-06-05 10:16:16 -0300

    Initial commit

---
+++ src/main/java/ar/com/xeven/crudrefugiospring/CrudRefugioSpringApplication.java
@@ -0,0 +1,13 @@
+package ar.com.xeven.crudrefugiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRefugioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRefugioSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/xeven/crudrefugiospring/CrudRefugioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.xeven.crudrefugiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRefugioSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
